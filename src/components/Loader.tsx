06e8192a32de360f75b7cf7201c032ce59a95b1c
10cyrilc <10cyrilc@gmail.com>
2025-05-11 00:43:10 +0530

    [UI] Enhance SEO and social media sharing with meta tags, add robots.txt and sitemap.xml

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const LoadingSpinner: React.FC = () => (
-    <div className="flex justify-center items-center h-full">
+    <div className="flex justify-center items-center h-full flex-col">
         <div className="flex space-x-2">
             <div className="w-3 h-3 bg-emerald-500 rounded-full animate-bounce"></div>
             <div className="w-3 h-3 bg-emerald-500 rounded-full animate-bounce delay-100"></div>
